468c4ce5156da9761fb11254eba20593c48b153a
Abdelrahman-abobkr <abdelrahman16174@gmail.com>
2023-06-04 01:56:47 -0700

    Adding Queryset on Cart Page And Order Summary in Checkout Page

--- core/views.py
+++ core/views.py
@@ -14,15 +14,31 @@ def home(request):
 
 
 def cart(request):
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {'get_total_items': 0, 'get_cart_total': 0}
     context = {
-
+        'items':items,
+        'order':order,
     }
     return render(request, 'cart.html', context)
 
 
 
-def checkout   (request):
+def checkout(request):
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {'get_total_items': 0, 'get_cart_total': 0}
     context = {
-
+        'items':items,
+        'order':order,
     }
     return render(request, 'checkout.html', context)
